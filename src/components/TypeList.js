1a6c446cae06694efc926b599313a5933469f225
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-12-03 15:41:49 +0200

    user can now filter pokemons by type

--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import PokemonDetail from './PokemonDetail';
 
 export default function TypeList() {
+  const [types, setTypes] = useState([]);
+  const [type, setType] = useState('');
+  const [pokemonsByType, setPokemonsByType] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/type/')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setTypes(json.results);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/type/${type}`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.pokemon);
+        setPokemonsByType(json.pokemon);
+      });
+    return function cleanUp() {
+      setPokemonsByType();
+    };
+  }, [type]);
+
+  function getPokemonsByType(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    setType(e.target.value);
+  }
+
+  let cardBody;
+
+  if (pokemonsByType !== undefined && pokemonsByType.length > 1) {
+    cardBody = (
+      <div className="row align-items-center">
+        {pokemonsByType.map((pokemon) => (
+          <div className="card-wrapper">
+            <PokemonDetail
+              pokemonDetails={pokemon.pokemon}
+              key={pokemon.id}
+            ></PokemonDetail>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    cardBody = '';
+  }
+
   return (
     <div>
-      <h3>The types will be here!</h3>
+      <div className="row">
+        {types.slice(0, types.length - 2).map((type) => (
+          <div className="col">
+            <button
+              onClick={getPokemonsByType}
+              className="type-button"
+              value={type.name}
+            >
+              <img
+                className="type-img"
+                src={`/pokemonTypesImg/${type.name}.png`}
+                alt="type-logo-button"
+              ></img>
+            </button>
+          </div>
+        ))}
+      </div>
+      {cardBody}
     </div>
   );
 }
